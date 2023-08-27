f796ae0036e83859e85fdb64b9b66dd112166071
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-08-27 17:46:08 -0300

    Comentários em PHP

---
+++ PHP/basico/comentarios.php
@@ -0,0 +1,15 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php 
+    echo "Estou no PHP";
+    // echo "Comentário";
+    # echo "Comentário";
+    /*
+    linha 01 do comentário
+    linha 02 do comentário
+    linha 03 do comentário
+
+    */
+
+?>
+<p>Primeiro Bloco</p>
--- PHP/index.php
+++ PHP/index.php
@@ -29,6 +29,9 @@
                         <li>
                             <a href="exercicio.php?dir=basico&file=css">Integração CSS</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
