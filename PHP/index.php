97ea6140301a17666db81a7a5d7ddb3d9f690e5d
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-08-28 03:16:11 -0300

    Desafio

--- PHP/index.php
+++ PHP/index.php
@@ -32,6 +32,9 @@
                         <li>
                             <a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP.</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">Desafio.</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
