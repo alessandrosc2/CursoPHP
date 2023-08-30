622d01aefff51eb278a66780c15a8e2df2148ede
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-08-30 02:14:35 -0300

    Operações Atitiméticas

--- PHP/tipos/aritimeticas.php
+++ PHP/tipos/aritimeticas.php
@@ -1,1 +1,16 @@
 <div class="titulo">Operações Aritiméticas</div>
+
+<?php
+    echo 7 / 4,'<br>';
+    echo intdiv(7, 4),'<br>';
+    echo round(7 / 4),'<br>';
+    echo 4 ** 2,'<br>';
+
+    echo '<p>Procedência de operadores</p>';
+
+    echo '<p>() => ** => / * % => + - </p>';
+
+?>
+
+
+
