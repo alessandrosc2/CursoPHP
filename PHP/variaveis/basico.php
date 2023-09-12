5566b076f92803ea7b88dca6a97c6bb3b93cf5e3
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-09-12 19:50:22 -0300

    PHP Variáveis

--- PHP/variaveis/basico.php
+++ PHP/variaveis/basico.php
@@ -1,6 +1,21 @@
 <div class="titulo">Variáveis</div>
 
 <?php
+$numeroA = 13;
+echo $numeroA, '<br>';
+var_dump($numeroA);
 
+echo '<br>';
 
+$a = 3;
+$b = 16;
+$soma = $a + $b;
+echo $soma;
+echo '<br>';
+
+$variavel = 10;
+echo '<br>' . $variavel;
+
+$variavel = 'Agora sou uma string';
+echo '<br>' . $variavel;
 ?>
