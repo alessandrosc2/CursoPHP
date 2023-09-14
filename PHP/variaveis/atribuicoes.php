2e61943cd539f61b09bfde1ba8c553c54edbd48c
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-09-14 03:01:05 -0300

    PHP INTERPOLAÇÃO 02

--- PHP/variaveis/atribuicoes.php
+++ PHP/variaveis/atribuicoes.php
@@ -1,6 +1,48 @@
 <div class="titulo">Atribuições</div>
 
 <?php
+$numero = 10;
+echo '<br>' . $numero;
+$numero--;
+echo '<br>' . $numero;
+$numero++;
+echo '<br>' . $numero;
+--$numero;
+echo '<br>' . $numero;
+$numero = $numero - 3;
+echo '<br>' . $numero;
+$numero = 20;
+echo '<br>' . $numero;
+$numero -= 6;
+echo '<br>' . $numero;
+$numero += 8;
+echo '<br>' . $numero;
+$numero *= 2;
+echo '<br>' . $numero;
+$numero /= 4;
+echo '<br>' . $numero;
+$numero **= 3;
+echo '<br>' . $numero;
+$numero .= 8; //concatenação
+echo '<br>' . $numero;
 
+$texto = 'Esse é um texto';
+echo '<br>' . $texto;
+
+$texto = $texto . ' qualquer';
+echo '<br>' . $texto;
+
+$texto .= ' de verdade!';
+echo '<br>' . $texto;
+
+//$valor = 'valor inexistente';
+echo '<br>' . $valor;
+$valores = $valor ?? 'valor default';
+echo '<br>' . $valores;
+
+//$valor = 'valor inexistente';
+echo '<br>' . $valor;
+$valores = $valor ?? 'valor default';
+echo '<br>' . $valores;
 
 ?>
