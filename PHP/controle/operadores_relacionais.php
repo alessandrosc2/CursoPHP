f10b8a0348b820627e81f66643c09470e763b9c1
Alessandro Cordeiro <alessandrosc2018@gmail.com>
2023-09-23 03:09:25 -0300

    final operadores relacionais

--- PHP/controle/operadores_relacionais.php
+++ PHP/controle/operadores_relacionais.php
@@ -29,4 +29,10 @@ var_dump(500 <=> 3);
 var_dump(50 <=> 50);
 var_dump(5 <=> 50);
 
+echo '<p class="divisao">Valores pode ser V ou F</p><hr>';
+var_dump(!!5);
+var_dump(!!0);
+var_dump(!!"");
+var_dump(!!" ");
+
 ?>
